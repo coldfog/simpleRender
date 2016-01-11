9d18792b2e3fc44ee19b319ffdc4bff7eb87e10a
Yuyao Feng <yyaofeng@gmail.com>
2016-01-11 22:42:00 +0800

    define Vertex and setup mesh for rendering

--- simple_render.py
+++ simple_render.py
@@ -5,6 +5,17 @@ import numpy as np
 WINDOW_W = 800
 WINDOW_H = 600
 
+vector = np.array
+
+
+class Vertex:
+
+    def __init__(self, pos=None, tex_coor=None, color=None, rhw=None):
+        self.pos = pos if pos is not None else np.zeros(4)
+        self.tex_coor = tex_coor if tex_coor is not None else np.zeros(2)
+        self.color = color if color is not None else np.zeros(3)
+        self.rhw = rhw if rhw else 0
+
 
 class Device:
 
@@ -17,6 +28,25 @@ class Device:
 if __name__ == '__main__':
     game_window = pyglet.window.Window(WINDOW_W, WINDOW_H)
     device = Device(WINDOW_W, WINDOW_H)
+
+    mesh = vector([
+        Vertex(pos=vector([1, -1,  1, 1]), tex_coor=vector([0, 0]),
+               color=vector([1.0, 0.2, 0.2]), rhw=1),
+        Vertex(pos=vector([1, -1,  1, 1]), tex_coor=vector([0, 1]),
+               color=vector([0.2, 1.0, 0.2]), rhw=1),
+        Vertex(pos=vector([1,  1,  1, 1]), tex_coor=vector([1, 1]),
+               color=vector([0.2, 0.2, 1.0]), rhw=1),
+        Vertex(pos=vector([1,  1,  1, 1]), tex_coor=vector([1, 0]),
+               color=vector([1.0, 0.2, 1.0]), rhw=1),
+        Vertex(pos=vector([1, -1, -1, 1]), tex_coor=vector([0, 0]),
+               color=vector([1.0, 1.0, 0.2]), rhw=1),
+        Vertex(pos=vector([1, -1, -1, 1]), tex_coor=vector([0, 1]),
+               color=vector([0.2, 1.0, 1.0]), rhw=1),
+        Vertex(pos=vector([1,  1, -1, 1]), tex_coor=vector([1, 1]),
+               color=vector([1.0, 0.3, 0.3]), rhw=1),
+        Vertex(pos=vector([1,  1, -1, 1]), tex_coor=vector([1, 0]),
+               color=vector([0.2, 1.0, 0.3]), rhw=1)
+    ])
 
     @game_window.event
     def on_draw():
