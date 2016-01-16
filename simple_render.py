877ce5d4695d5789d995d166959b8daee2342d6f
Yuyao Feng <yyaofeng@gmail.com>
2016-01-16 11:56:55 +0800

    move _get_martix & _normalize to Device class as static method

--- simple_render.py
+++ simple_render.py
@@ -9,11 +9,12 @@ from skimage.draw import line
 WINDOW_W = 800
 WINDOW_H = 600
 
-vector = lambda x: np.array(x, dtype='float32')
+
+def vector(x):
+    return np.array(x, dtype='float32')
 
 
 class Vertex:
-
     def __init__(self, pos=None, tex_coor=None, color=None, rhw=None):
         self.pos = pos if pos is not None else np.zeros(4)
         self.tex_coor = tex_coor if tex_coor is not None else np.zeros(2)
@@ -21,61 +22,7 @@ class Vertex:
         self.rhw = rhw if rhw else 0
 
 
-def _get_martix(scale=None, rotate=None, translate=None):
-    '''
-    scale: (scalex, scaley, scalez)
-    rotate: (x, y, z, theta). <x, y, z> indicate the axis rotated by,
-            theta is the degree of rotation
-    translate: (translatex, translatey, translatez)
-    '''
-    res = np.eye(4)
-
-    if scale:
-        tmp_matrix = np.eye(4)
-        tmp_matrix[0, 0] = scale[0]
-        tmp_matrix[1, 1] = scale[1]
-        tmp_matrix[2, 2] = scale[2]
-        res = np.dot(res, tmp_matrix)
-
-    if rotate:
-        tmp_matrix = np.eye(4)
-        x, y, z = _normalize(vector(rotate[:3]))
-        theta = rotate[3]
-
-        cos = np.cos(theta)
-        one_sub_cos = 1 - cos
-        sin = np.sin(theta)
-        tmp_matrix[0, 0] = cos + one_sub_cos * x ** 2
-        tmp_matrix[0, 1] = one_sub_cos * x * y - sin * z
-        tmp_matrix[0, 2] = one_sub_cos * x * z + sin * y
-        tmp_matrix[1, 0] = one_sub_cos * y * x + sin * z
-        tmp_matrix[1, 1] = cos + one_sub_cos * y ** 2
-        tmp_matrix[1, 2] = one_sub_cos * y * z - sin * x
-        tmp_matrix[2, 0] = one_sub_cos * z * x - sin * y
-        tmp_matrix[2, 1] = one_sub_cos * z * y + sin * x
-        tmp_matrix[2, 2] = cos + one_sub_cos * z ** 2
-        res = np.dot(res, tmp_matrix)
-
-    if translate:
-        tmp_matrix = np.eye(4)
-        tmp_matrix[0, 3] = translate[0]
-        tmp_matrix[1, 3] = translate[1]
-        tmp_matrix[2, 3] = translate[2]
-        res = np.dot(res, tmp_matrix)
-
-    return res
-
-
-def _normalize(v):
-    n = np.linalg.norm(v)
-    if n != 0:
-        return v / n
-    else:
-        return v
-
-
 class Device:
-
     def __init__(self, width, height):
         self._frame_buffer = np.zeros((height, width, 4), dtype='uint8')
 
@@ -84,17 +31,66 @@ class Device:
 
         self._world_trans = np.eye(4)
         self._view_trans = np.eye(4)
+        self._projection_trans = np.eye(4)
+        self._trans = np.eye(4)
         self.set_perspective(
-            np.pi * 0.5,  float(self.width) / self.height, 1.0, 500)
+            np.pi * 0.5, float(self.width) / self.height, 1.0, 500)
+
+    @staticmethod
+    def make_transform(scale=None, rotate=None, translate=None):
+        """
+        scale: (scalex, scaley, scalez)
+        rotate: (x, y, z, theta). <x, y, z> indicate the axis rotated by,
+                theta is the degree of rotation
+        translate: (translatex, translatey, translatez)
+        """
+        res = np.eye(4)
+
+        if scale:
+            tmp_matrix = np.eye(4)
+            tmp_matrix[0, 0] = scale[0]
+            tmp_matrix[1, 1] = scale[1]
+            tmp_matrix[2, 2] = scale[2]
+            res = np.dot(res, tmp_matrix)
+
+        if rotate:
+            tmp_matrix = np.eye(4)
+            x, y, z = Device._normalize(vector(rotate[:3]))
+            theta = rotate[3]
+
+            cos = np.cos(theta)
+            one_sub_cos = 1 - cos
+            sin = np.sin(theta)
+            tmp_matrix[0, 0] = cos + one_sub_cos * x ** 2
+            tmp_matrix[0, 1] = one_sub_cos * x * y - sin * z
+            tmp_matrix[0, 2] = one_sub_cos * x * z + sin * y
+            tmp_matrix[1, 0] = one_sub_cos * y * x + sin * z
+            tmp_matrix[1, 1] = cos + one_sub_cos * y ** 2
+            tmp_matrix[1, 2] = one_sub_cos * y * z - sin * x
+            tmp_matrix[2, 0] = one_sub_cos * z * x - sin * y
+            tmp_matrix[2, 1] = one_sub_cos * z * y + sin * x
+            tmp_matrix[2, 2] = cos + one_sub_cos * z ** 2
+            res = np.dot(res, tmp_matrix)
+
+        if translate:
+            tmp_matrix = np.eye(4)
+            tmp_matrix[0, 3] = translate[0]
+            tmp_matrix[1, 3] = translate[1]
+            tmp_matrix[2, 3] = translate[2]
+            res = np.dot(res, tmp_matrix)
+
+        return res
+
+    @staticmethod
+    def _normalize(vec):
+        n = np.linalg.norm(vec)
+        return vec / n if n != 0 else vec
 
     def set_camera(self, eye, at, up):
 
-        z = _normalize((at - eye)[:3])
-        print z
-        x = _normalize(np.cross(up[:3], z))
-        print x
-        y = _normalize(np.cross(z, x))
-        print y
+        z = self._normalize((at - eye)[:3])
+        x = self._normalize(np.cross(up[:3], z))
+        y = self._normalize(np.cross(z, x))
 
         tmp_matrix = np.eye(4)
         tmp_matrix[0, :3] = x
@@ -186,21 +182,21 @@ if __name__ == '__main__':
 
     # define the mesh of box
     mesh = [
-        Vertex(pos=vector([1, -1,  1, 1]), tex_coor=vector([0, 0]),
+        Vertex(pos=vector([1, -1, 1, 1]), tex_coor=vector([0, 0]),
                color=vector([1.0, 0.2, 0.2]), rhw=1),
-        Vertex(pos=vector([-1, -1,  1, 1]), tex_coor=vector([0, 1]),
+        Vertex(pos=vector([-1, -1, 1, 1]), tex_coor=vector([0, 1]),
                color=vector([0.2, 1.0, 0.2]), rhw=1),
-        Vertex(pos=vector([-1,  1,  1, 1]), tex_coor=vector([1, 1]),
+        Vertex(pos=vector([-1, 1, 1, 1]), tex_coor=vector([1, 1]),
                color=vector([0.2, 0.2, 1.0]), rhw=1),
-        Vertex(pos=vector([1,  1,  1, 1]), tex_coor=vector([1, 0]),
+        Vertex(pos=vector([1, 1, 1, 1]), tex_coor=vector([1, 0]),
                color=vector([1.0, 0.2, 1.0]), rhw=1),
         Vertex(pos=vector([1, -1, -1, 1]), tex_coor=vector([0, 0]),
                color=vector([1.0, 1.0, 0.2]), rhw=1),
         Vertex(pos=vector([-1, -1, -1, 1]), tex_coor=vector([0, 1]),
                color=vector([0.2, 1.0, 1.0]), rhw=1),
-        Vertex(pos=vector([-1,  1, -1, 1]), tex_coor=vector([1, 1]),
+        Vertex(pos=vector([-1, 1, -1, 1]), tex_coor=vector([1, 1]),
                color=vector([1.0, 0.3, 0.3]), rhw=1),
-        Vertex(pos=vector([1,  1, -1, 1]), tex_coor=vector([1, 0]),
+        Vertex(pos=vector([1, 1, -1, 1]), tex_coor=vector([1, 0]),
                color=vector([0.2, 1.0, 0.3]), rhw=1)
     ]
 
@@ -211,13 +207,15 @@ if __name__ == '__main__':
 
     d = 1
 
+
     @game_window.event
     def on_draw():
         game_window.clear()
         device.clear_frame_buffer()
 
         global d
-        device.set_world_trans(_get_martix(rotate=(1, 1, 1, d/np.pi*180)))
+        trans = device.make_transform(rotate=(1, 1, 1, d / np.pi * 180))
+        device.set_world_trans(trans)
         d += 0.0005
         d %= 180
         # draw box
@@ -232,8 +230,10 @@ if __name__ == '__main__':
             'RGBA', device.width * 4, device.get_frame_buffer_str())
         frame.blit(0, 0)
 
+
     def update(dt):
         pass
+
 
     pyglet.clock.schedule_interval(update, 1 / 60.0)
 
